4731727c65d6eb5f7b14cb41619f8b1f73a08e74
Michal Podolak <michal.podolak@physter.com>
2019-08-22 02:29:19 +0200

    edit

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-module.exports = require('./lib/Server.js');
+#!/usr/bin/env node
+const Server = require('./lib/Server.js');
+const websocketOptions = {
+    port: 8080
+};
+const guacdOptions = {
+    port: 4822,
+    host: '127.0.0.1'
+};
+const clientOptions = {
+    crypt: {
+        cypher: 'AES-256-CBC',
+        key: 'MySuperSecretKeyForParamsToken12'
+    },
+    log: {
+        level: 'DEBUG'
+    }
+};
+const guacServer = new Server(websocketOptions, guacdOptions, clientOptions);
